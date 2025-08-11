850411f52f61fe6babbb72f8a4744cd9acb8c3a4
paulscalise1 <93959622+paulscalise1@users.noreply.github.com>
2025-08-10 23:51:57 -0500

    RGB565 encoding

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -3,14 +3,29 @@
 // Author: Paul Scalise
 
 #include <WiFi.h>
+//#include <ESP8266WiFi.h>
 #include <esp_now.h>
+//#include <espnow.h>
 #include <strings.h>
 #include <Arduino.h>
 #include <esp_wifi.h>
+#include <TJpg_Decoder.h>
 
 static const size_t RX_BUFFER_SIZE = 32 * 1024;
 static uint8_t  packetBuf[RX_BUFFER_SIZE];
 static size_t   packetLen = 0;
+
+bool tjpg_render_callback(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t *bitmap) {
+  size_t byteCount = size_t(w) * h * 2;
+  Serial.write((uint8_t*)bitmap, byteCount); // Serial.write
+  return true;
+}
+
+void decodeJPEGtoRGB565(const uint8_t* jpegBuf, size_t jpegLen) { // row major, left to right, top to bottom
+  if (!TJpgDec.drawJpg(0, 0, jpegBuf, jpegLen)) {
+     Serial.println(F("JPEG decode failed!"));
+  }
+}
 
 void printMAC(){
   uint8_t baseMac[6];
@@ -29,8 +44,10 @@ void ESP_NOW_Init(){
   WiFi.disconnect();
 
   if (esp_now_init() != ESP_OK) {
-    Serial.println("*** Error initializing ESP-NOW");
-    while (true) { delay(1000); }
+    while (true) { 
+    	Serial.println("*** Error initializing ESP-NOW");
+    	delay(1000);
+    }
   }
   esp_now_register_recv_cb(onDataRecv);
 }
@@ -45,9 +62,8 @@ void onDataRecv(const esp_now_recv_info_t* info, const uint8_t* data, int len){
 
   // JPEG frames start “IMG” and end 0xFF 0xD9
   if (packetLen >= 3 && strncasecmp((const char*)packetBuf, "IMG", 3) == 0) {
-    if (packetLen >= 2 &&
-        packetBuf[packetLen-2] == 0xFF &&
-        packetBuf[packetLen-1] == 0xD9) {
+    if (packetLen >= 2 && packetBuf[packetLen-2] == 0xFF && packetBuf[packetLen-1] == 0xD9) {
+    	/*
       Serial.println(F("\n---- BEGIN JPEG HEX DUMP ----"));
       for (size_t i = 0; i < packetLen; i++) {
         Serial.printf("%02X ", packetBuf[i]);
@@ -55,6 +71,8 @@ void onDataRecv(const esp_now_recv_info_t* info, const uint8_t* data, int len){
       Serial.println();
       Serial.println(F("----  END JPEG HEX DUMP  ----"));
       Serial.println("Frame done, waiting for next…");
+      */
+      decodeJPEGtoRGB565(packetBuf + 3, expect - 3);
       packetLen = 0;
     }
   }
